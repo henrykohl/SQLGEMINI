af0b6d967d01b835dc0428c3327027eb51c9dbf1
Henry Hsu <43148403+henrykohl@users.noreply.github.com>
2024-07-04 21:33:51 -0500

    file updated

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ Implementation
 
 * requirements.txt
 
-* <font color="red">.env (Github不會保存此檔，clone 此 reposiotry 一定要重新建立)
+* <font color="red">.env (Github不會保存此檔，clone 此 reposiotry 一定要重新建立)</font>
 > ``` 
 > GOOGLE_API_KEY = '...'
 > ```
@@ -40,3 +40,21 @@ pip install -r requiremetns.txt
 > ```bash
 > >streamlit run sql.py
 > ```
+
+* 四個問題
+> tell me all the students name from Data science class
+> 
+> tell me Sudhanshu section
+> 
+> Tell me the class of Vikash and Dipesh
+> 
+> Tell me the student name from section A and B
+
+* 使用 第2 table 與 第2 data
+> 刪除 `student.db
+>
+> 執行
+> ```python
+> >python sqlite.py
+> >streamlit run sql.py
+> ```  
--- sqlite.py
+++ sqlite.py
@@ -11,6 +11,10 @@ cursor=connection.cursor()
 table_info="""
 Create table STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),SECTION VARCHAR(25));
 """
+## 第2 table
+# table_info="""
+# Create table STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),SECTION VARCHAR(25),MARKS INT);
+# """
 cursor.execute(table_info)
 
 ## Insert Some more records
@@ -20,6 +24,13 @@ cursor.execute('''Insert Into STUDENT values('Sudhanshu','Data Science','B')''')
 cursor.execute('''Insert Into STUDENT values('Darius','Data Science','A')''')
 cursor.execute('''Insert Into STUDENT values('Vikash','DEVOPS','A')''')
 cursor.execute('''Insert Into STUDENT values('Dipesh','DEVOPS','A')''')
+
+## 第2 data
+# cursor.execute('''Insert Into STUDENT values('Krish','Data Science','A',90)''')
+# cursor.execute('''Insert Into STUDENT values('Sudhanshu','Data Science','B',100)''')
+# cursor.execute('''Insert Into STUDENT values('Darius','Data Science','A',86)''')
+# cursor.execute('''Insert Into STUDENT values('Vikash','DEVOPS','A',50)''')
+# cursor.execute('''Insert Into STUDENT values('Dipesh','DEVOPS','A',35)''')
 
 ## Display ALL the records
 
